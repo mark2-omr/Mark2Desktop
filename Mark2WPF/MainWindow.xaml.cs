a619e2bbde2b3ce13815089522c9cb6f4dea1c9b
Hiroaki Kawata <maemaewater@gmail.com>
2020-11-19 00:34:14 +0900

    Recognize呼び出し

--- Mark2WPF/MainWindow.xaml.cs
+++ Mark2WPF/MainWindow.xaml.cs
@@ -46,5 +46,72 @@ namespace Mark2WPF
                 imageFolderPathTextBlock.Text = survey.folderPath;
             }
         }
+
+        private async void OpenCsvButton_Click(object sender, RoutedEventArgs e)
+        {
+            survey.csvPath = null;
+            csvPathTextBlock.Text = "";
+
+            var picker = new OpenFileDialog();
+
+            try
+            {
+                if (picker.ShowDialog() == true)
+                {
+                    survey.csvPath = picker.FileName;
+                    await survey.SetupPositions();
+                    csvPathTextBlock.Text = survey.csvPath;
+                }
+            } catch
+            {
+                MessageBox.Show("Error");
+            }
+            
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //survey.areaThreshold = areaThresholdSlider.Value / 100.0;
+            //survey.colorThreshold = colorThresholdSlider.Value / 100.0;
+            survey.areaThreshold = 0.4f;
+            survey.colorThreshold = 0.1f;
+
+            //if (survey.folder == null || survey.csv == null)
+            //{
+            //    return;
+            //}
+
+            await survey.SetupOutputFolders();
+            //startButton.IsEnabled = false;
+
+            Task taskMain = new Task(async () =>
+            {
+                System.Diagnostics.Debug.WriteLine("Recognizing");
+
+                await survey.Recognize( (i, max) =>
+                {
+                     Dispatcher.Invoke(() => {
+                        progressBar.Value = (100.0 / (double)(max)) * (i + 1);
+                     });
+                    //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    //{
+                    //    progressBar.Value = (100.0 / (double)(max)) * (i + 1);
+                    //});
+                });
+
+                System.Diagnostics.Debug.WriteLine("Finished");
+
+                //await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                //{
+                //    // if (resultCSV != null)
+                //    {
+                //        startButton.IsEnabled = true;
+                //        saveButton.IsEnabled = true;
+                //        SaveCsv();
+                //    }
+                //});
+            });
+            taskMain.Start();
+        }
     }
 }
