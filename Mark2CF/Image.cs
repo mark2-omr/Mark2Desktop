666be380bf2b8e477657aef7e344ff22823aacf3
Hiroaki Kawata <maemaewater@gmail.com>
2021-02-01 17:42:53 +0900

    環境によって切り替える

---
+++ Mark2CF/Image.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mark2CF
+{
+    public class Rgba32
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+        public byte A;
+
+        public Rgba32()
+        {
+
+        }
+
+        public Rgba32(float r, float g, float b)
+        {
+
+        }
+
+        static public Rgba32 ParseHex(string hex)
+        {
+            return new Rgba32();
+        }
+    }
+    public class Image<T>
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public T this[int x, int y]
+        {
+            get
+            {
+                return default(T);
+            }
+
+            set
+            {
+
+            }
+        }
+    }
+}
