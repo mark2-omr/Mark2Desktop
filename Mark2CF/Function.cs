ee8217febc79685fa2cb2bfdc2779552af9e951c
Hiroaki Kawata <maemaewater@gmail.com>
2021-04-04 21:31:32 +0900

    HandleAsync追加

---
+++ Mark2CF/Function.cs
@@ -0,0 +1,54 @@
+﻿using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Mark2;
+using Google.Cloud.Storage.V1;
+
+namespace Mark2CF
+{
+    class Function : IHttpFunction
+    {
+        public async Task HandleAsync(HttpContext context)
+        {
+            string keyPath = "";
+            if (context.Request.Query.ContainsKey("key") == true)
+            {
+                keyPath = "/" + context.Request.Query["key"];
+            }
+
+            string bucketName = "mark2-storage-dev";
+            string folderPath = "images" + keyPath;
+
+            //var storage = StorageClient.Create();
+            //var objects = storage.ListObjects(bucketName);
+
+
+            string output = "";
+            //foreach (var file in objects)
+            //{
+            //    output += file.Name + "#";
+            //}
+
+
+            Survey survey = new Survey();
+            survey.areaThreshold = 0.4;
+            survey.colorThreshold = 0.1;
+
+            survey.folderPath = "gs://" + bucketName + "/" + folderPath;
+            survey.csvPath = "gs://" + bucketName + "/csv" + keyPath + "/input.csv";
+
+            // キーのパスにファイルがなければ処理を中止するようにする
+            survey.SetupPositions();
+
+            await survey.Recognize((i, max) =>
+            {
+
+            });
+
+            await context.Response.WriteAsync(output);
+        }
+    }
+}
